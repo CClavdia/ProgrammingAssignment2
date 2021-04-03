c0137630bf561c8a290dc503b665cd2868078ada
CClavdia <mclaserr12@gmail.com>
2021-04-03 15:58:06 -0600

    Claudia file

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+# makeCacheMatrix is a new function that give back a list of functions
+# one of the objetives is to store a martix and cached value of the inverse of the 
+# matrix.
+# This functions also contains the following functions:
+#getMatrix      get the value of a matrix 
+#setMatrix      set the value of a matrix
+#cacheInverse   get the cahced value (inverse of the matrix)
+#getInverse     get the cahced value (inverse of the matrix
+
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+# holds the cached value or could give NULL if nothing is cached
+# initially nothing is cached so set it to NULL
